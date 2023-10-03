3d005621a8789158202ee29f5ec6b57fb8b0f787
TommyArias <arias.tommy1009@gmail.com>
2023-10-03 15:32:48 +0200

    script api

--- script.js
+++ script.js
@@ -1,46 +1,45 @@
-console.log("ok")
+console.log("ok");
+const clientId = '740a08f1a64a40ddbd331b44c3f79b99';
+const playerContainer = document.getElementById('spotify-player');
+const playButton = document.getElementById('play-button');
 
-// Remplacez 'VOTRE_CLE_API' par votre clé API
-const apiKey = 'AIzaSyDnakAV4_fHv8iTTGJGFUXiVYwd9ZB7ANk';
-const videoId = 'YltjliK0ZeA'; // Remplacez par l'ID de la vidéo YouTube
+let player;
 
-// Faites une requête à l'API YouTube Data
-fetch(`https://www.googleapis.com/youtube/v3/videos?id=${videoId}&key=${apiKey}&part=snippet`, {
-    method: 'GET'
-})
-    .then(response => response.json())
-    .then(data => {
-        // Obtenez l'URL de la vidéo
-        const videoUrl = `https://www.youtube.com/watch?v=${data.items[0].id}`;
-        console.log(videoUrl);
-    
-        // Mettez à jour l'URL de la source audio
-        const audioSource = document.getElementById('audioSource');
-        audioSource.src = videoUrl;
-    
-        // Rechargez le lecteur audio avec la nouvelle source
-        const audioPlayer = document.getElementById('audioPlayer');
-        audioPlayer.load();
-    })
-    
-    .catch(error => {
-        console.error(error);
-    });
+window.onSpotifyWebPlaybackSDKReady = () => {
+  player = new Spotify.Player({
+    name: 'Mon Lecteur Spotify',
+    getOAuthToken: callback => {
+      // Définissez la fonction pour obtenir un jeton d'accès Spotify ici
+      // Vous devrez implémenter une authentification Spotify, par exemple via l'API OAuth 2.0.
+      // Une fois obtenu, appelez le callback avec le jeton d'accès.
+      // Pour plus d'informations, consultez la documentation de l'API Spotify.
+    },
+    volume: 0.5
+  });
 
-// Récupérez une référence vers le bouton et l'élément audio
-const playButton = document.getElementById('playButton');
-const audioPlayer = document.getElementById('audioPlayer');
-
-// Écoutez l'événement de clic sur le bouton
-playButton.addEventListener('click', function () {
-    // Vérifiez si le lecteur audio est en pause
-    if (audioPlayer.paused) {
-        // Si c'est en pause, démarrez la lecture
-        audioPlayer.play();
-        playButton.textContent = 'Pause'; // Changez le texte du bouton en "Pause"
-    } else {
-        // Si cela joue, mettez en pause
-        audioPlayer.pause();
-        playButton.textContent = 'Lancer la lecture'; // Changez le texte du bouton en "Lancer la lecture"
+  // Connectez le lecteur
+  player.connect().then(success => {
+    if (success) {
+      console.log('Le lecteur Spotify est connecté');
     }
+  });
+
+  // Écoutez les changements d'état du lecteur
+  player.addListener('player_state_changed', state => {
+    console.log('Nouvel état du lecteur :', state);
+  });
+
+  // Chargez une piste Spotify (utilisez 'spotify:track' pour une piste)
+  player.addListener('ready', ({ device_id }) => {
+    console.log('Le lecteur est prêt avec l\'identifiant du périphérique', device_id);
+  });
+};
+
+// Ajoutez un gestionnaire d'événements au bouton pour lancer/arrêter la musique
+playButton.addEventListener('click', () => {
+  if (player) {
+    player.togglePlay();
+  } else {
+    console.log('Le lecteur Spotify n\'est pas encore prêt.');
+  }
 });
