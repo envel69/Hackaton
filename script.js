b672742b91517b5494a7cc58fa6ebf4b48099c0a
TommyArias <arias.tommy1009@gmail.com>
2023-10-04 12:04:06 +0200

    base blind test back end

--- script.js
+++ script.js
@@ -1,45 +1,44 @@
-console.log("ok");
-const clientId = '740a08f1a64a40ddbd331b44c3f79b99';
-const playerContainer = document.getElementById('spotify-player');
-const playButton = document.getElementById('play-button');
+var audio = document.getElementById("myAudio");
+var songs = ["audio/KIKESA - 42 JOURS.mp3", "audio/Nekfeu - On Verra (Clip Officiel).mp3", "audio/Des Histoires a Raconter.mp3", "audio/Trophée Vald.mp3"];
+var currentSongIndex = 0;
 
-let player;
+function playRandomSongAndShowButtons() {
+    var randomIndex = Math.floor(Math.random() * songs.length);
+    currentSongIndex = randomIndex;
+    audio.src = songs[currentSongIndex];
+    audio.play();
+    showSongButtons();
+}
 
-window.onSpotifyWebPlaybackSDKReady = () => {
-  player = new Spotify.Player({
-    name: 'Mon Lecteur Spotify',
-    getOAuthToken: callback => {
-      // Définissez la fonction pour obtenir un jeton d'accès Spotify ici
-      // Vous devrez implémenter une authentification Spotify, par exemple via l'API OAuth 2.0.
-      // Une fois obtenu, appelez le callback avec le jeton d'accès.
-      // Pour plus d'informations, consultez la documentation de l'API Spotify.
-    },
-    volume: 0.5
-  });
-
-  // Connectez le lecteur
-  player.connect().then(success => {
-    if (success) {
-      console.log('Le lecteur Spotify est connecté');
+function showSongButtons() {
+    var songButtonsDiv = document.getElementById("songButtons");
+    songButtonsDiv.innerHTML = "";
+    for (var i = 0; i < songs.length; i++) {
+        var button = document.createElement("button");
+        button.textContent = getSongTitle(songs[i]);
+        button.onclick = createButtonOnClickHandler(i);
+        songButtonsDiv.appendChild(button);
     }
-  });
+}
 
-  // Écoutez les changements d'état du lecteur
-  player.addListener('player_state_changed', state => {
-    console.log('Nouvel état du lecteur :', state);
-  });
+function getSongTitle(songFileName) {
+    // Extrait le nom du fichier (sans l'extension) comme titre de la chanson.
+    var fileNameParts = songFileName.split("/");
+    var fileName = fileNameParts[fileNameParts.length - 1];
+    return fileName.replace(".mp3", "");
+}
 
-  // Chargez une piste Spotify (utilisez 'spotify:track' pour une piste)
-  player.addListener('ready', ({ device_id }) => {
-    console.log('Le lecteur est prêt avec l\'identifiant du périphérique', device_id);
-  });
-};
+function createButtonOnClickHandler(index) {
+    return function() {
+        checkAnswer(index);
+    };
+}
 
-// Ajoutez un gestionnaire d'événements au bouton pour lancer/arrêter la musique
-playButton.addEventListener('click', () => {
-  if (player) {
-    player.togglePlay();
-  } else {
-    console.log('Le lecteur Spotify n\'est pas encore prêt.');
-  }
-});
+function checkAnswer(index) {
+    if (index === currentSongIndex) {
+        document.getElementById("responseMessage").innerHTML = "Bonne réponse!";
+        playRandomSongAndShowButtons();
+    } else {
+        document.getElementById("responseMessage").innerHTML = "Mauvaise réponse!";
+    }
+}
