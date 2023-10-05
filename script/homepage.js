fa14be9dc130ab15aa3da2c3e90f2b17b5635869
sleo <leo.sallet@ynov.com>
2023-10-05 09:46:24 +0200

    ajout script

---
+++ script/homepage.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const countdown = document.getElementById("countdown");
+    let seconds = 30;
+  
+    function updateCountdown() {
+      countdown.textContent = seconds + " secondes";
+    }
+  
+    function countdownInterval() {
+      updateCountdown();
+      if (seconds === 0) {
+        clearInterval(interval);
+        countdown.textContent = "Terminé!";
+      } else {
+        seconds--;
+      }
+    }
+  
+    const interval = setInterval(countdownInterval, 1000);
+  
+    countdownInterval();
+  });
+  
